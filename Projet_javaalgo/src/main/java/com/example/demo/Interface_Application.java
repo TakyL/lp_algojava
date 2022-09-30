74dad5c247a72c733619e3875de2f49f835884ef
TakyL <takinou57@gmail.com>
2022-09-30 12:22:32 +0200

    Modifications mineures sur la vue,le ctrl et le fxml

--- Projet_javaalgo/src/main/java/com/example/demo/Interface_Application.java
+++ Projet_javaalgo/src/main/java/com/example/demo/Interface_Application.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Interface_Application extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Interface_Application.class.getResource("interface_principale.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 480);
+        stage.setTitle("Interface!");
         stage.setScene(scene);
         stage.show();
     }
---
+++ Projet_javaalgo/src/main/java/com/example/demo/Interface_Ctrl.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class Interface_Ctrl {
+    @FXML
+    private Button btn_TvM; //Texte vers Morse
+
+    @FXML
+    private Button btn_MvT; //Morse vers Texte
+    @FXML
+    private TextArea saisie;    //Sasie utilisateur et resultat
+
+    @FXML
+    protected void onHelloButtonClick() {
+
+    }
+
+    @FXML
+    protected void Conv_Mvt(){}
+
+    @FXML
+    protected void Conv_TvM(){};
+}
