7c734a42d9e8cfbadaf70fb8240b185e712175af
Cliffy57 <73029436+Cliffy57@users.noreply.github.com>
2022-11-12 17:42:40 +0100

    Module rename

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,12 @@
-module com.example.demo {
+module fr.iut {
     requires javafx.controls;
     requires javafx.fxml;
-
-
-
 
     exports fr.iut.vue;
     opens fr.iut.vue to javafx.fxml;
     exports fr.iut.controleur;
     opens fr.iut.controleur to javafx.fxml;
-
+    exports  fr.iut.modeles;
+    opens fr.iut.modeles to javafx.fxml;
 
 }
